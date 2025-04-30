16cdbec77134155a614976aafd4c4a38346b7a6e
Rave1231 <ugurrne@icloud.com>
2025-04-30 23:17:35 +0300

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const supabase = supabase.createClient(
-  'https://[PROJE_URL].supabase.co',
-  '[ANON_PUBLIC_KEY]'
+  'https://[PROJE_URL].supabase.co',https://zuzkkxpteffksmdkqifq.supabase.co
+  '[ANON_PUBLIC_KEY]'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1emtreHB0ZWZma3NtZGtxaWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMzYzMDUsImV4cCI6MjA2MTYxMjMwNX0.QjnQl4bHuNVGW02pL9L9MjaZcAnks5lP8q22-yfukTA
 );
 // Yukarıdaki iki satırı kendi Supabase projenle değiştir
